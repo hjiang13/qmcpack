e653d8b6b3476b36c05227cfb7d8d96b13613cc6
Hailong Jiang <hjiang13@kent.edu>
2023-04-27 17:39:37 +0000

    Edit bash file to compile qmcpack

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
-#!/bin/bash
-echo Configuring and building froom inside the build directory.
-echo Check the results of the CMake configuration to ensure that the preferred
-echo compilers and libraries have been selected. See README and documentation 
-echo for guidance.
-cd build; cmake ..; make; cd ..
+module load gcc/12.1.0
+
+cd build
+
+cmake -G Ninja -DENABLE_OFFLOAD=ON \
+        -DENABLE_CUDA=OFF \
+        -DCMAKE_CUDA_ARCHITECTURES=70 \
+        -DENABLE_OMP_TASKLOOP=OFF \
+        -DQMC_OMP=ON    \
+        -DCMAKE_C_COMPILER=/home/hailong.jiang/llvm-9-project/llvm-9-install-release/bin/clang \
+        -DCMAKE_CXX_COMPILER=/home/hailong.jiang/llvm-9-project/llvm-9-install-release/bin/clang++ \
+        -DENABLE_MPI=OFF \
+        -DCMAKE_CXX_FLAGS=--gcc-toolchain=/soft/compilers/gcc/9.5.0/x86_64-suse-linux/  \
+        -DQMC_MPI=OFF \
+        -DUSE_OBJECT_TARGET=ON \
+        -DOFFLOAD_ARCH=sm_70 \
+        -DOFFLOAD_TARGET=nvptx64-nvidia-cuda \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DUSE_OBJECT_TARGET=ON \
+        -DCMAKE_C_FLAGS=--gcc-toolchain=/soft/compilers/gcc/9.5.0/x86_64-suse-linux/ ../
